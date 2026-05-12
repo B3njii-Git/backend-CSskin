eca8e24b38ba3ac2d2f84ecaef9263f8cae8c9b3
Benjamin Guerrero <benguerrerov@duocuc.cl>
2026-05-11 23:54:28 -0400

    inicializacion de spring

---
+++ src/main/java/com/example/backend_skins/BackendSkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_skins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_skins/BackendSkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_skins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
